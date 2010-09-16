4fcf7d02e15c7d3664f661f17201fe92340e2683
tazarnai <tarik@azarnait.ch>
2010-09-16 19:31:30 +0200

    commit myfirstMavenProject

---
+++ myapp/src/main/java/ch/tarik/app/App.java
@@ -0,0 +1,18 @@
+package ch.tarik.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String who = whoDidIFuck();
+        System.out.println( "Hello World!" + who );
+    }
+
+    public static String whoDidIFuck() {
+        return "Dini Mueter";
+    }
+}
